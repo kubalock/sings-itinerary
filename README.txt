40e0375834c9220104eebe0d5e64d9041fab7fa2
Grzegorz <46896109+kubalock@users.noreply.github.com>
2022-07-13 13:43:25 +0100

    Initial commit
    Alpha tests begin here

---
+++ README.txt
@@ -0,0 +1,27 @@
+=== SinGS Itinerary ===
+Contributors: Grzegorz Kubala
+Requires at least: 5.3.2
+Tested up to: 5.3.2
+Stable tag: 5.3.2
+
+Here is a short description of the plugin.  This should be no more than 150 characters.  No markup here.
+
+== Description ==
+
+This plugin allows your customers to enter their booking references with password in order to see their Itineraries
+
+== Installation & Setup ==
+
+Typically one of our team members would send you the installation .zip file.
+
+- In the WordPress dashboard under 'Plugins' menu, click the 'Add New' button.
+- Click on 'Upload Plugin', choose the file we've sent you and click 'Install Now'
+- Our plugin should appear in the 'Installed Plugins' page. Please make sure it's activated.
+- Go to 'Sings Itinerary' page and paste in your 'Sings Widget ID' in the existing field. You can find your Widget ID by
+going to your branch setup in SinGS.
+- Widget is available for you to place anywhere on your website. You can add it in the 'Widgets' page in dashboard.
+
+== Changelog ==
+
+= 0.0.1 =
+* Initial commit - alpha tests begin here
---
+++ sings-itinerary.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/kubalock/sings-itinerary
+ * @since             0.0.1
+ * @package           Sings_Itinerary
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Sings Itinerary
+ * Plugin URI:        https://github.com/kubalock/sings-itinerary
+ * Description:       This plugin allows your customers to enter their booking references with password in order to see their Itineraries
+ * Version:           0.0.1
+ * Author:            Grzegorz Kubala
+ * Author URI:
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       Sings Itinerary
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'SINGS_ITINERARY_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-sings-itinerary-activator.php
+ */
+function activate_sings_itinerary() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sings-itinerary-activator.php';
+	Sings_Itinerary_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-sings-itinerary-deactivator.php
+ */
+function deactivate_sings_itinerary() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sings-itinerary-deactivator.php';
+	Sings_Itinerary_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_sings_itinerary' );
+register_deactivation_hook( __FILE__, 'deactivate_sings_itinerary' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-sings-itinerary.php';
+
+/**
+ * Include our updater below - it'll enable updating from Git
+ */
+
+if( ! class_exists( 'Sings_Updater' ) ){
+    include_once( plugin_dir_path( __FILE__ ) . 'update.php' );
+}
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+
+$updater = new Sings_Updater( __FILE__ );
+$updater->set_username( 'kubalock' );
+$updater->set_repository( 'sings-itinerary' );
+$updater->initialize(); // initialize the updater
+
+function run_sings_itinerary() {
+
+	$plugin = new Sings_Itinerary();
+	$plugin->run();
+
+}
+run_sings_itinerary();
+
